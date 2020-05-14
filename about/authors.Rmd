c78fdfde313504e57865834cbf552ec17f539a6c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-13 23:42:36 -0700

    Added some tail end bits and pieces for book formatting.

---
+++ about/authors.Rmd
@@ -0,0 +1,8 @@
+# Contributors
+
+### *Aaron Lun* {-}
+
+An ancient interdimensional horror who has recently awoken from his aeons-long slumber. 
+His true name cannot be pronounced by the human tongue and his true form cannot be grasped by the human mind. 
+It is said that, when the stars are right, he will herald the return of the Old Ones to plunge the world into darkness once more. 
+In the meantime, he maintains about 20 Bioconductor packages for analyzing a range of genomics data modalities.
--- deploy.R
+++ deploy.R
@@ -5,18 +5,20 @@ dir.create(builddir, showWarnings=FALSE)
 unlink(list.files(builddir, pattern="Rmd$", full.names=TRUE))
 
 ordering <- c(
-    "introduction.Rmd", 
-    "bulkref.Rmd",
-    "scref.Rmd",
-    "diagnostics.Rmd", 
-    "multiple.Rmd",
-    "ontology.Rmd", 
-    "advanced.Rmd"
+    "topics/introduction.Rmd", 
+    "topics/bulkref.Rmd",
+    "topics/scref.Rmd",
+    "topics/diagnostics.Rmd", 
+    "topics/multiple.Rmd",
+    "topics/ontology.Rmd", 
+    "topics/advanced.Rmd",
+    "about/authors.Rmd",
+    "about/bibliography.Rmd"
 )
 
 for (x in seq_along(ordering)) {
-    new.name <- sprintf("%02d-%s", x, ordering[x])
-    file.copy(file.path("topics", ordering[x]), file.path(builddir, new.name))
+    new.name <- sprintf("%02d-%s", x, basename(ordering[x]))
+    file.copy(ordering[x], file.path(builddir, new.name))
 }
 
 for (y in list.files("sundries", full.names=TRUE)) {
